84bdae3b0a82edbe216dd73485f20177cec3f8cc
Anton Holovchenko <golvcenkoanton@gmail.com>
2024-06-04 13:14:46 +0200

    Update deploy scripts, and mocks

--- deploy/00-deploy-MockV3Aggreator.js
+++ deploy/00-deploy-MockV3Aggreator.js
@@ -1,20 +1,24 @@
 const { network } = require("hardhat")
-const { developmentChains } = require("../../helper-hardhat-config")
+const { developmentChains } = require("../helper-hardhat-config")
 
 module.exports = async({deployments, getNamedAccounts}) => {
     const { deployer } = await getNamedAccounts()
     const { deploy, log } = deployments
-    const DECIMALS = 8 // 
-    const INITIAL_ANSWER = 200000000000 // at what number the prceFeed is starting
+    const DECIMALS = 18 
+    const INITIAL_ANSWER = 200000000000000000000 // at what number the prceFeed is starting
 
     if (developmentChains.includes(network.name)) {
-        console.log(`============ Deploying Mocks on local network ============`)
+        
+        console.log(`\n============ Deploying Mocks on local network ============\n`)
+        
         await deploy("MockV3Aggregator", {
+            contract:"MockV3Aggregator",
             from: deployer,
             log: true,
             args: [DECIMALS, INITIAL_ANSWER]
         })
-        console.log(`Successfully deployed mocks`)
+        
+        console.log(`\n============Successfully deployed mocks============\n`)
     } 
 }
 
